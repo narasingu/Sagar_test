5baff88cf8ac3a300a4cf5173b97f2500eea4696
rat <rat@rat.com>
2019-06-05 10:52:50 +0530

    latest updated code for flipkart

--- src/Module1/openurl.java
+++ src/Module1/openurl.java
@@ -1,10 +1,14 @@
 package Module1;
 
+import java.awt.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class openurl {
 //test 
@@ -14,16 +18,23 @@ public class openurl {
 		 System.setProperty("webdriver.chrome.driver", exepath);
 		 
 		  WebDriver driver = new ChromeDriver();
+		
 		driver.manage().timeouts().setScriptTimeout(10,TimeUnit.SECONDS);
 		  //launch chrome and direct it to the URL
-		  driver.get("http://www.testingtreasure.com");
-		  driver.findElement(By.xpath("//*[@id=\"header-inner\"]/div/h1/a")).click();
-		  //testcases executes
-		  //maximize window
 		  driver.manage().window().maximize();
+		  driver.get("https://www.google.com/");
+		  //open the page
 		  
-		  //close browser
-		  driver.close();
+		  driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input")).sendKeys("flipkart");
+		  
+		
+
+		 
+	
+		
+		
+		
+	
 	}
 
 }
